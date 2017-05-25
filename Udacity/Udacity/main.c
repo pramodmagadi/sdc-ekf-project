84b564f8ff6c481676122a14b4f803bcb7619248
pramodmagadi <pramods286@gmail.com>
2017-05-24 22:08:12 -0400

    Kalman Filter Implementation

---
+++ Udacity/Udacity/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Udacity
+//
+//  Created by Pramod Magadi on 5/2/17.
+//  Copyright © 2017 sdc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
